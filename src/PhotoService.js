595eca34e42fe1688947af322aafaf50698f54e3
rrrafalsz <rrrafalsz@gmail.com>
2020-10-16 12:11:50 +0200

    do not use svg images

--- src/PhotoService.js
+++ src/PhotoService.js
@@ -4,7 +4,7 @@ class PhotoService {
      * @returns {string}
      */
     getRandomPhotoSrc() {
-        const photos = ["react.svg", "6lm10a.png", "angular.png", "banana.jpg", "bedraggled.jpg", "cudgel.jpg", "dog-mask.jpg", "hal-cropped.jpg", "laravel.png", "trip01.png", "trip02.png", "trip03.png"];
+        const photos = ["react.png", "6lm10a.png", "angular.png", "banana.jpg", "bedraggled.jpg", "cudgel.jpg", "dog-mask.jpg", "hal-cropped.jpg", "laravel.png", "trip01.png", "trip02.png", "trip03.png"];
         const photoId = Math.floor(Math.random() * photos.length);
         return photos[photoId];
     }
