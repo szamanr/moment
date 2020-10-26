7d633e70983536e90825b7ae2430cf343cdf220d
rrrafalsz <rrrafalsz@gmail.com>
2020-10-26 11:15:30 +0100

    fix header button styling

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './Header.css';
-import {FaLongArrowAltLeft, FaStar, FaBars, FaUserCircle} from 'react-icons/fa'
+import {FaCogs, FaLongArrowAltLeft, FaStar, FaUserCircle} from 'react-icons/fa'
 
 function Header() {
     return (
         <header className="Header">
             <span className="button brand" id='header-back'><FaLongArrowAltLeft/></span>
             <h3 id='header-title'>moment #001</h3>
-            <span className="button brand"  id='header-favorite'><FaStar/></span>
-            <span className="button brand" id='header-menu'><FaBars/></span>
+            <div id="header-buttons">
+                <span className="button brand" id='header-favorite'><FaStar/></span>
+                <span className="button brand" id='header-menu'><FaCogs/></span>
+            </div>
             <small className="description">22 april 2020</small>
             <div className="Users">
-                <FaUserCircle/> <FaUserCircle/> <FaUserCircle/> <FaUserCircle/>
+                <span className="user-icon"><FaUserCircle/></span>
+                <span className="user-icon"><FaUserCircle/></span>
+                <span className="user-icon"><FaUserCircle/></span>
+                <span className="user-icon"><FaUserCircle/></span>
             </div>
         </header>
     );
