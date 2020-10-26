fb439bd3719e90e202f35d228d187b8ba6a9d7c9
rrrafalsz <rrrafalsz@gmail.com>
2020-10-26 12:47:12 +0100

    install firebase

--- src/index.js
+++ src/index.js
@@ -5,15 +5,26 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import PhotoService from './PhotoService.js';
 import NoteService from "./NoteService";
+import firebase from "firebase";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyD5IMuc6iK6KTGFQwbZAc7c7QkFGzZV1MQ",
+    authDomain: "moment-a16db.firebaseapp.com",
+    databaseURL: "https://moment-a16db.firebaseio.com",
+    projectId: "moment-a16db",
+    storageBucket: "moment-a16db.appspot.com",
+    messagingSenderId: "897838693187",
+    appId: "1:897838693187:web:09b69181f3a9dd67ff4df6"
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App
-        photoService={new PhotoService()}
-        noteService={new NoteService()}
-    />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App
+            photoService={new PhotoService()}
+            noteService={new NoteService()}
+        />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
