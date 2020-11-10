194b77dc4eca24c273c3201295e12af734cb9db2
rrrafalsz <rrrafalsz@gmail.com>
2020-11-10 10:45:47 +0100

    fetch moment list from db

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,13 +1,44 @@
 import {Link} from "react-router-dom";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import firebase from "firebase";
 
-const Dashboard = () => (
-    <nav>
-        <ul>
-            <li><Link to="/moment/00001">moment 00001</Link></li>
-            <li><Link to="/moment/00002">moment 00002</Link></li>
-        </ul>
-    </nav>
-);
+const Dashboard = function () {
+    const db = firebase.database().ref('moments');
+    const [moments, setMoments] = useState([]);
+
+    // fetch a list of moments
+    useEffect(() => {
+        db.on('value', (snapshot) => {
+            let momentList = [];
+            const items = snapshot.val() ?? {};
+
+            Object.keys(items).forEach((id) => {
+                let title = items[id].meta.title ?? 'moment ' + id;
+                momentList.push({
+                    id,
+                    title
+                });
+            });
+
+            setMoments(momentList);
+        });
+
+        return function cleanup() {
+            db.off('value');
+        }
+    });
+
+    const momentLinks = moments.map(({id, title}) => (
+        <li key={id}><Link to={"/moment/" + id}>{title}</Link></li>)
+    );
+
+    return (
+        <nav>
+            <ul>
+                {momentLinks}
+            </ul>
+        </nav>
+    );
+}
 
 export default Dashboard;
