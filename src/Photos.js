2e693812dba74a2d48490e8c9c996a306ada8667
rrrafalsz <rrrafalsz@gmail.com>
2020-11-18 12:20:02 +0000

    rewrite photos component using hooks

--- src/Photos.js
+++ src/Photos.js
@@ -2,27 +2,21 @@ import React from "react";
 import './Photos.css';
 import {FaPlus} from "react-icons/fa";
 
-class Photos extends React.Component {
-    size = {w: "150px", h: "150px"};
-    defaultMetadata = {
+function Photos(props) {
+    const size = {w: "150px", h: "150px"};
+    const defaultMetadata = {
         cacheControl: 'public, max-age=900, immutable'
     };
-
-    constructor(props) {
-        super(props);
-
-        this.removePhoto = this.removePhoto.bind(this);
-    }
 
     /**
      * removes an item from the list
      * @param id
      * @param e
      */
-    removePhoto = (id, e) => {
+    const removePhoto = (id, e) => {
         e.preventDefault();
 
-        this.props.removePhoto(id);
+        props.removePhoto(id);
     }
 
     /**
@@ -31,40 +25,38 @@ class Photos extends React.Component {
      * @param id
      * @param e
      */
-    focusedPhoto = (id, e) => {
+    const focusedPhoto = (id, e) => {
         e.preventDefault();
 
-        this.props.setFocused(e.target, id, 'photos');
+        props.setFocused(e.target, id, 'photos');
     }
 
-    addPhotoElement = (
+    const addPhotoElement = (
         <div className="photo photo-add">
             <label htmlFor="photo-add" className="button brand">
                 <FaPlus/>
             </label>
             <input id="photo-add" name="photo-add" type="file" hidden multiple={true}
                    onChange={(e) => {
-                       this.props.addPhoto(e.target.files, this.defaultMetadata);
+                       props.addPhoto(e.target.files, defaultMetadata);
                    }}/>
         </div>
     );
 
-    render() {
-        const photoElements = this.props.photos.map((photo) => {
-            return (
-                <div key={photo.id} className="photo" onClick={this.focusedPhoto.bind(this, photo.id)}>
-                    <img src={photo.src} width={this.size.w} height={this.size.h} alt={photo.alt}/>
-                </div>
-            );
-        });
-
+    const photoElements = props.photos.map((photo) => {
         return (
-            <div className="photos">
-                {photoElements}
-                {this.addPhotoElement}
+            <div key={photo.id} className="photo" onClick={focusedPhoto.bind(this, photo.id)}>
+                <img src={photo.src} width={size.w} height={size.h} alt={photo.alt}/>
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="photos">
+            {photoElements}
+            {addPhotoElement}
+        </div>
+    );
 }
 
 export default Photos;
