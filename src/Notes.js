3cc300b9d1501e416f3594c914dd917be6e8c26d
rrrafalsz <rrrafalsz@gmail.com>
2020-11-18 12:16:17 +0000

    rewrite notes component using hooks

--- src/Notes.js
+++ src/Notes.js
@@ -2,46 +2,40 @@ import React from "react";
 import "./Notes.css";
 import {FaPlus} from "react-icons/fa";
 
-class Notes extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-
-        this.addNote = this.addNote.bind(this);
-    }
+function Notes(props) {
 
     /**
      * adds a new dummy note
      */
-    addNote = () => {
-        const note = this.props.noteService.getRandomNote();
-        this.props.addNote(note);
+    const addNote = () => {
+        const note = props.noteService.getRandomNote();
+        props.addNote(note);
     }
 
-    addNoteElement = (
-        <li key="new" onClick={this.addNote} className="note note-add">
+    const addNoteElement = (
+        <li key="new" onClick={addNote} className="note note-add">
             <span className="button brand">
                 <FaPlus/>
             </span>
         </li>
     );
 
-    render() {
-        const notesElement = this.props.notes.map((note) => {
-            return (
-                <li key={note.id} className="note" onClick={() => {
-                    this.props.setFocused(note, note.id, 'notes')
-                }}>
-                    <span>{note.title}</span>
-                </li>
-            );
-        });
+    const notesElement = props.notes.map((note) => {
         return (
-            <ul className="notes">
-                {notesElement}
-                {this.addNoteElement}
-            </ul>
+            <li key={note.id} className="note" onClick={() => {
+                props.setFocused(note, note.id, 'notes')
+            }}>
+                <span>{note.title}</span>
+            </li>
         );
-    }
+    });
+
+    return (
+        <ul className="notes">
+            {notesElement}
+            {addNoteElement}
+        </ul>
+    );
 }
 
 export default Notes;
