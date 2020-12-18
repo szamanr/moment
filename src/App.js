4743995535b1a23d48240b070959819116e4fcdc
rrrafalsz <rrrafalsz@gmail.com>
2020-12-18 12:10:47 +0100

    reorganise routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {Route, useHistory, useParams} from "react-router-dom";
+import React, {useEffect} from "react";
+import {Route} from "react-router-dom";
 import Moment from "./Moment";
 import Dashboard from "./Dashboard";
 import * as FirestoreService from "./services/firestore";
@@ -7,26 +7,21 @@ import Header from "./Header";
 import "./App.css";
 
 function App() {
-    const history = useHistory();
-
-    // TODO: implement login
-    const signIn = function () {
-        FirestoreService.authenticate()
-            .then(() => {
-                history.push('/dashboard');
-            });
-    }
+    useEffect(() => {
+        FirestoreService.authenticate();
+    }, []);
 
     return (
         <div className="App">
-            <Route exact path="/dashboard" component={Dashboard}/>
+            <Route exact path={["/", "/dashboard"]}>
+                <Dashboard/>
+            </Route>
+
             <Route path="/moment/:momentId">
                 <Header className="Header"/>
                 <Moment/>
             </Route>
-            <Route exact path="/">
-                {signIn()}
-            </Route>
+
             <footer>
                 [footer]
             </footer>
